1d51d720c41cc3cc766f760f3d3a27d410f45907
kxtemas <71146102+kxtemas@users.noreply.github.com>
2021-03-27 22:38:42 -0700

    rest debug fixed

--- app/DTO.php
+++ app/DTO.php
@@ -1,24 +1,52 @@
 <?php
 namespace App;
+use Illuminate\Database\Eloquent\Model;
 
-class DTO implements \JsonSerializable 
-{
+use JsonSerializable;
+
+
+class DTO implements JsonSerializable {
 	public $data;
-	
+	public $errorCode;
+	public $errorMessage;
 
-	public function __construct() 
-	{
-		$this->data = [];
-        $this->errorCode = null;
-		$this->errorMessage = null;
-	}
+	public function __construct($errorCode, $errorMessage, $data)
+    {
+        $this->errorCode = $errorCode;
+        $this->errorMessage = $errorMessage;
+        $this->data = $data;
+    }
 
-	public function jsonSerialize() 
-	{
-        // return [
-        //     'username' => $this->username,
-        //     'password' => $this->password
-        // ];
-        return get_object_vars($this);
-    }	
+	 public function jsonSerialize()
+    {
+       return get_object_vars($this);
+    }
+
+
+    //getters
+
+
+    /**
+     * @return mixed
+     */
+    public function getErrorCode()
+    {
+        return $this->errorCode;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getErrorMessage()
+    {
+        return $this->errorMessage;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
 }
